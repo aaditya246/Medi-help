8769766902ec2bd8a92f4490317a21a12fd03d41
Aaditya <aaditya.20234001@mnnit.ac.in>
2025-11-12 11:56:21 +0530

    add files

---
+++ backend/models/doctorModel.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const doctorSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    image: { type: String, required: true },
+    speciality: { type: String, required: true },
+    degree: { type: String, required: true },
+    experience: { type: String, required: true },
+    about: { type: String, required: true },
+    available: { type: Boolean, default: true },
+    fees: { type: Number, required: true },
+    address: { type: Object, required: true },
+    date: { type: Number, required: true },
+    slots_booked: { type: Object, default: {} },
+  },
+  { minimize: false }     // by this we can make doctor data with empty object present in slots_booked .
+  //   if it is true we can not make data with empty objects
+);
+
+const doctorModel =
+  mongoose.models.doctor || mongoose.model("doctor", doctorSchema);
+
+export default doctorModel;
